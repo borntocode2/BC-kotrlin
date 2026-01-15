01c80f6621c6e2f8383dea058c88090af44bb1bb
ralph <sanghwaj222@gmail.com>
2026-01-15 19:52:38 +0900

    test(SC): test file create

--- src/main/kotlin/SimpleCalculator.kt
+++ src/main/kotlin/SimpleCalculator.kt
@@ -1,6 +1,6 @@
 package com.example
 
-object Calculator {
+object SimpleCalculator {
     fun add(a: Int, b: Int): Int = a + b
     fun subtract(a: Int, b: Int): Int = a - b
     fun multiply(a: Int, b: Int): Int = a / b
---
+++ src/test/kotlin/SimpleCalculatorTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import kotlin.test.Test
+
+@DisplayName("Simple Calculator 테스트")
+class SimpleCalculatorTest {
+    @Test
+    fun addition() {
+
+    }
+}
