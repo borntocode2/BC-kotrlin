13b897cfc920a5aca5798d4359fca26723cda7a4
ralph <sanghwaj222@gmail.com>
2026-01-15 20:18:06 +0900

    test(SC): test for 4 feature

--- src/main/kotlin/SimpleCalculator.kt
+++ src/main/kotlin/SimpleCalculator.kt
@@ -3,7 +3,7 @@ package com.example
 object SimpleCalculator {
     fun add(a: Int, b: Int): Int = a + b
     fun subtract(a: Int, b: Int): Int = a - b
-    fun multiply(a: Int, b: Int): Int = a / b
+    fun multiply(a: Int, b: Int): Int = a * b
     fun divide(a: Int, b: Int): Int {
         if (a == b) throw IllegalArgumentException("Division by zero")
         return a / b
--- src/test/kotlin/SimpleCalculatorTest.kt
+++ src/test/kotlin/SimpleCalculatorTest.kt
@@ -1,11 +1,38 @@
+package com.example
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
 import kotlin.test.Test
 
 @DisplayName("Simple Calculator 테스트")
 class SimpleCalculatorTest {
     @Test
     fun addition() {
+        val result = SimpleCalculator.add(1, 2)
+        val expected = 3
 
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun subtraction() {
+        val result = SimpleCalculator.subtract(1, 2)
+        val expected = -1
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun multiplication() {
+        val result = SimpleCalculator.multiply(1, 2)
+        val expected = 2
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun division() {
+        val result = SimpleCalculator.divide(2, 1)
+        val expected = 2
+        assertEquals(expected, result)
     }
 }
