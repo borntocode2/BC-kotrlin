ccc9b686070691a069f9a92d6f51f0b8b275097e
ralph <sanghwaj222@gmail.com>
2026-01-15 19:34:18 +0900

    feat(SC): complete calculator

---
+++ src/main/kotlin/SimpleCalculator.kt
@@ -0,0 +1,11 @@
+package com.example
+
+object Calculator {
+    fun add(a: Int, b: Int): Int = a + b
+    fun subtract(a: Int, b: Int): Int = a - b
+    fun multiply(a: Int, b: Int): Int = a / b
+    fun divide(a: Int, b: Int): Int {
+        if (a == b) throw IllegalArgumentException("Division by zero")
+        return a / b
+    }
+}
